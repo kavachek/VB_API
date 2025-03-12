4563536a5787f638b623e50d165433045b4d4ae4
kavac <kavachek2@gmail.com>
2025-03-12 20:28:44 +0300

    Видоизменил ключ

--- settings/config.py
+++ settings/config.py
@@ -1,8 +1,11 @@
 # Ключ для взаимодействия с вб.
-API_KEY = ('eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjUwMjE3djEiLCJ0eXAiOiJKV1Qif'
-           'Q.eyJlbnQiOjEsImV4cCI6MTc1NzQ3MjQ0NCwiaWQiOiIwMTk1ODVhYy1iOWE'
-           'wLTdiOWYtOWVjMS1kNDMxYzBjZmIwZWMiLCJpaWQiOjE0OTU5MTQzMiwib2lk'
-           'Ijo0MDc0ODY1LCJzIjoxMDczNzQ5MTE4LCJzaWQiOiIwYzM2M2EwZC1mMWQ4L'
-           'TQ5MmMtYWZkNi02YzUxZjU2MWEzNWYiLCJ0IjpmYWxzZSwidWlkIjoxNDk1OT'
-           'E0MzJ9.obtwfao7sB9OBeoOzPHhDtiRDY1lMudIgGUucOlcOpncBUS-h7sTrx'
-           'AV18s3WqBPJPVTN6zxHx58dn3QGgn-zg')
+API_KEY = ('eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjUwMjE3dj'
+           'EiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6M'
+           'Tc1NzQ3MjQ0NCwiaWQiOiIwMTk1ODVhYy1iOWEwLTd'
+           'iOWYtOWVjMS1kNDMxYzBjZmIwZWMiLCJpaWQiOjE0O'
+           'TU5MTQzMiwib2lkIjo0MDc0ODY1LCJzIjoxMDczNzQ'
+           '5MTE4LCJzaWQiOiIwYzM2M2EwZC1mMWQ4LTQ5MmMtY'
+           'WZkNi02YzUxZjU2MWEzNWYiLCJ0IjpmYWxzZSwidWl'
+           'kIjoxNDk1OTE0MzJ9.obtwfao7sB9OBeoOzPHhDtiR'
+           'DY1lMudIgGUucOlcOpncBUS-h7sTrxAV18s3WqBPJP'
+           'VTN6zxHx58dn3QGgn-zg')
