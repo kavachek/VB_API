5e3df3598692d286e4586269f1f5a1e72bc39763
kavac <kavachek2@gmail.com>
2025-03-12 20:28:22 +0300

    Исправил данные для получения информации.

--- url_queries/url.py
+++ url_queries/url.py
@@ -2,23 +2,23 @@
 BASIC_URL = {
     'Orders': {
         'url': 'https://statistics-api.wildberries.ru/api/v1/supplier/orders',
-        'params_template': {'date_from': None, 'flag': 0} # Заказы
+        'params_template': {'dateFrom': None, 'flag': 0} # Заказы
     },
     'Sales': {
         'url': 'https://statistics-api.wildberries.ru/api/v1/supplier/sales',
-        'params_template': {'date_from': None, 'flag': 0} # Продажи
+        'params_template': {'dateFrom': None, 'flag': 0} # Продажи
     },
     'Remains': {
         'url': 'https://statistics-api.wildberries.ru/api/v1/supplier/stocks',
-        'params_template': {'date_from': None} # Остатки
+        'params_template': {'dateFrom': None} # Остатки
     },
     'Refunds': {
         'url': 'https://statistics-api.wildberries.ru/api/v1/supplier/returns',
-        'params_template': {'date_from': None} # Возвраты
+        'params_template': {'dateFrom': None} # Возвраты
     },
     'Sales_Report': {
         'url': 'https://statistics-api.wildberries.ru/api/v1/supplier/reportDetailByPeriod',
-        'params_template': {'date_from': None, 'date_to': None} # Отчет о продажах
+        'params_template': {'dateFrom': None, 'dateTo': None} # Отчет о продажах
     }
 }
 
@@ -26,18 +26,18 @@ BASIC_URL = {
 ADD_URL = {
     'Product_Information': {
         'url': 'https://suppliers-api.wildberries.ru/api/v2/stocks',
-        'params_template': {'date_from': None} # Информация о продукте
+        'params_template': {'dateFrom': None} # Информация о продукте
     },
     'Product_rating': {
         'url': 'https://suppliers-api.wildberries.ru/api/v1/supplier/products/rating',
-        'params_template': {'date_from': None} # Рейтинг продукта
+        'params_template': {'dateFrom': None} # Рейтинг продукта
     },
     'Delivery_Information': {
         'url': 'https://suppliers-api.wildberries.ru/api/v1/supplier/incomes',
-        'params_template': {'date_from': None} # Информация о поставках
+        'params_template': {'dateFrom': None} # Информация о поставках
     },
     'Information_about_payments': {
         'url': 'https://suppliers-api.wildberries.ru/api/v1/supplier/payments',
-        'params_template': {'date_from': None} # Информация о выплатах
+        'params_template': {'dateFrom': None} # Информация о выплатах
     }
 }
