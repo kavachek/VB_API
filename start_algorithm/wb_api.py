53bf956f32768ca7b68b014bc921cc39be58528b
kavac <kavachek2@gmail.com>
2025-03-12 20:57:05 +0300

    Полный сбор всех данных и их группировка по классификациям.

--- start_algorithm/wb_api.py
+++ start_algorithm/wb_api.py
@@ -1,13 +1,37 @@
-import requests
-import pandas as pd
-from url_queries.url import *
+from settings.config import API_KEY
+from url_queries.url import BASIC_URL
+from start_algorithm.collecting_information import get_wb_data, filter_data, save_to_excel
 
 
-def get_data(date_from, date_to):
+def main(start_date, end_date, **filter_params):
     """
-    :param date_from: указывает с какой даты начать получать данные
-    :param date_to: указывает до какой даты закончить получать данные
-    :return:
+    Главная функция для получения, фильтрации и сохранения данных.
+
+    :param start_date: Дата начала периода.
+    :param end_date: Дата конца периода.
+    :param filter_params: Дополнительные фильтры.
     """
+    all_data = []
 
+    # Получаем данные для каждого запроса
+    for name, in_request in BASIC_URL.items():
+        # Выполняем запрос к API
+        data = get_wb_data(API_KEY, in_request, start_date, end_date, **filter_params)
 
+        # Если данные получены, фильтруем их
+        if data:
+            filtered_data = filter_data(data, **filter_params)
+            all_data.extend(filtered_data)
+        else:
+            continue
+
+    # Сохраняем все данные в один файл
+    if all_data:
+        save_to_excel(all_data, 'wildberries_data.xlsx')
+
+# Глобальные переменные
+date_from = '2024-09-15'
+date_to = '2024-09-15'
+filters = {'orderType': 'Клиентский'}
+
+main(date_from, date_to, **filters)
