d0a44da9d891d69bcd1ce55e2bab6c3dafbb143e
kavac <kavachek2@gmail.com>
2025-03-16 15:10:19 +0300

    Обновил код, убрав предупреждения.

--- export_to_excel/reports.py
+++ export_to_excel/reports.py
@@ -1,27 +1,33 @@
+import os
 from datetime import datetime
-from classification_of_data.analyzing_everything import analyzing_sales
-from export_excel import export_to_excel_with_chart
+from classification_of_data.analyzing_everything import analyzing_sales, analyzing_stocks
+from export_excel import export_sales_report, export_stocks_report
 from export_to_excel.save_path import get_save_path
+
 
 def get_date_range():
     while True:
-        input_start_date = input("Введите начальную дату (ГГГГ-ММ-ДД): ")
-        input_end_date = input("Введите конечную дату (ГГГГ-ММ-ДД): ")
+        input_start_date_str = input("Введите начальную дату (ГГГГ-ММ-ДД): ")
+        input_end_date_str = input("Введите конечную дату (ГГГГ-ММ-ДД): ")
 
         try:
-            start_date = datetime.strptime(input_start_date, "%Y-%m-%d")
-            end_date = datetime.strptime(input_end_date, "%Y-%m-%d")
-            return None, start_date, end_date
+            input_start_date = datetime.strptime(input_start_date_str, "%Y-%m-%d")
+            input_end_date = datetime.strptime(input_end_date_str, "%Y-%m-%d")
+            return input_start_date, input_end_date
         except ValueError:
             print("Неверный формат даты! Используйте формат ГГГГ-ММ-ДД.")
 
+
 if __name__ == "__main__":
-    error_message, start_date, end_date = get_date_range()
-    # Путь к файлу
+    start_date, end_date = get_date_range()
     save_path = get_save_path()
 
-    # Анализируем данные
-    analysis_results = analyzing_sales(start_date, end_date)
+    os.makedirs(save_path, exist_ok=True)
 
-    if analysis_results: export_to_excel_with_chart(analysis_results, save_path, filename="cities_revenue.xlsx")
-    else: print("Не удалось выполнить анализ. Возможно, данные по выбранным датам отсутствуют в базе данных.")
+    # Анализируем данные
+    sales_results = analyzing_sales(start_date, end_date)
+    stocks_results = analyzing_stocks()
+
+    # Экспортируем каждый отчет отдельно
+    export_sales_report(sales_results, save_path, start_date, end_date)
+    export_stocks_report(stocks_results, save_path, start_date, end_date)
