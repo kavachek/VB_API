3d34794ed4179858e5b64d0759a3708fdd0ab01d
kavac <kavachek2@gmail.com>
2025-03-16 14:53:09 +0300

    Обновил код.

--- classification_of_data/analyzing_everything.py
+++ classification_of_data/analyzing_everything.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def analyzing_sales(start_date=None, end_date=None):
     """
-    Анализ продаж, заказов, остатков и возвратов с визуализацией и уведомлениями.
+    Анализ продаж, заказов, остатков и возвратов.
     """
     db_file = '../classification_of_data/wildberries.db'
     conn = sqlite3.connect(db_file)
@@ -47,4 +47,26 @@ def analyzing_sales(start_date=None, end_date=None):
         'prices_by_city': prices_by_city,
         'prices_by_country': prices_by_country,
         'city_country_orders': city_country_orders,
+        'sales_data': df
+    }
+
+
+def analyzing_stocks():
+    """
+    Анализ остатков.
+    """
+    db_file = '../classification_of_data/wildberries.db'
+    conn = sqlite3.connect(db_file)
+    query = "SELECT * FROM wildberries_data;"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+
+    if df is None or df.empty: return None
+
+    stocks_by_city = df.groupby('warehouseName')['quantity'].sum().reset_index()
+    stocks_by_city.columns = ['warehouseName', 'total_stock']
+
+    return {
+        'stocks_by_city': stocks_by_city,
+        'stocks_data': df
     }
